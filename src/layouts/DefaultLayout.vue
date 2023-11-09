e68bd762c1ace2f52172fb65aa31cd3791beaf29
christopher <christopher.pher39@gmail.com>
2023-11-09 11:33:27 -0600

    added vercel.json

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -4,7 +4,7 @@ import Footer from '../components/Footer.vue';
 </script>
 
 <template>
-   <!--  <NavegationVue></NavegationVue> -->
+    <NavegationVue></NavegationVue>
     <router-view />
     <Footer></Footer>
 </template>
